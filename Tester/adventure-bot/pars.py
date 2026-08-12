bd102f5729ad052b7cced118c1610398ae4c37c8
Somebody553 <BerTim09@yandex.ru>
2026-08-13 01:47:30 +0400

    echopars-proxyfix

--- Tester/adventure-bot/pars.py
+++ Tester/adventure-bot/pars.py
@@ -3,9 +3,13 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import CommandStart
+from aiogram.client.session.aiohttp import AiohttpSession
 from dotenv import load_dotenv
-load_dotenv() # подтягивает переменные из .env
-bot = Bot(os.getenv("BOT_TOKEN"))
+load_dotenv()
+proxy_url="socks5://eu8buz:zEhk8F@168.80.73.57:8000"
+session = AiohttpSession(proxy=proxy_url) if proxy_url else AiohttpSession()
+TOKEN=os.getenv("BOT_TOKEN")
+bot = Bot(token=TOKEN, session=session)
 dp = Dispatcher()
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
